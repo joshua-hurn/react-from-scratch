c398dd5b708e50a53654f3f535edbc42f77b4298
joshua-hurn <joshua.hurn@hotmail.com>
2018-10-29 20:16:57 -0500

    fixed overcommit

---
+++ test-react-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container d-flex">
+      <header className="row">
+        <h1 className="mb-5">Mike Landry</h1>
+      </header>
+      <p>Accomplished musician for over 30 years.</p>
+    </div>
+  );
+};
+
+export default App;
